55518a7999901fceb095fad7d73a3258bb5fa6b5
bgiobbe <bgiobbe@gmail.com>
2022-10-22 11:33:24 -0400

    When the button is clicked, select a random image and display it

--- js/script.js
+++ js/script.js
@@ -6,10 +6,33 @@ const img = document.querySelector(".img");
 const loremPicsum = "https://picsum.photos/v2/list";
 
 const getImage = async function () {
+    // Get 100 images from Lorem Picsum
     const res = await fetch(loremPicsum + "?limit=100");
     const images = await res.json();
     console.log(images);
+
+    selectRandomImage(images);
 };
 
-getImage();
+const selectRandomImage = function (images) {
+    const index = Math.floor(Math.random() * images.length);
+    const randomImg = images[index];
+    console.log(randomImg);
+
+    displayImage(randomImg);
+};
+
+const displayImage = function (randomImage) {
+    const author = randomImage.author;
+    const imageAddress = randomImage.download_url;
+
+    authorSpan.innerText = author;
+    img.src = imageAddress;
+    imgDiv.classList.remove("hide");
+};
+
+// Display a random image when button is clicked
+button.addEventListener("click", function () {
+    getImage();
+});
 
