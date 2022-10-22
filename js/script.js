0d6c714ac967de2da31f2be00f536ac7e911841c
bgiobbe <bgiobbe@gmail.com>
2022-10-22 00:19:06 -0400

    Request images from LoremPicsum API

--- js/script.js
+++ js/script.js
@@ -2,3 +2,14 @@ const button = document.querySelector("button");
 const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
+
+const loremPicsum = "https://picsum.photos/v2/list";
+
+const getImage = async function () {
+    const res = await fetch(loremPicsum + "?limit=100");
+    const images = await res.json();
+    console.log(images);
+};
+
+getImage();
+
